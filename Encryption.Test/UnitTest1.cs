7d4462a0610c3544417a5b7d47cc69839d23b975
anit3k <steffen.halberg@gmail.com>
2022-11-02 19:45:30 +0100

    Added Nunit test

---
+++ Encryption.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Encryption.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
